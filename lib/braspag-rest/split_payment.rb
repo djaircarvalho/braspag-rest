e5a12381a2ed311e6f4e7cddc06eae8b219fcd80
Joao Mafra <joaomafra21@gmail.com>
2018-10-11 13:43:03 -0300

    Added property fares in split_payment.rb

--- lib/braspag-rest/split_payment.rb
+++ lib/braspag-rest/split_payment.rb
@@ -2,5 +2,6 @@ module BraspagRest
   class SplitPayment < Hashie::IUTrash
     property :subordinate_merchant_id, from: 'SubordinateMerchantId'
     property :amount, from: 'Amount'
+    property :fares, from: 'Fares', with: ->(values) { BraspagRest::Fare.new(values) }
   end
 end
