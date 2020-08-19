e5860d9aa7274a4f44debffc4ebfebd97a47089e
development <development@development-pc.bonitour.com.br>
2020-08-19 16:05:53 -0300

    Adicionado novas infos para fraude

--- lib/braspag-rest/customer.rb
+++ lib/braspag-rest/customer.rb
@@ -7,6 +7,8 @@ module BraspagRest
     property :identity_type, from: 'IdentityType'
     property :email, from: 'Email'
     property :ip_address, from: 'IpAddress'
+    property :phone, from: 'Phone'
+    property :birthdate, from: 'Birthdate'
     property :address, from: 'Address', with: ->(values) { BraspagRest::Address.new(values) }
     property :delivery_address, from: 'DeliveryAddress', with: ->(values) { BraspagRest::Address.new(values) }
     property :billing_address, from: 'BillingAddress', with: ->(values) { BraspagRest::Address.new(values) }
